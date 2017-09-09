641583103ebbfd5e9cfbdc72b13114e91172cc60
Joseph M. O'Connor <tiluser@yahoo.com>
2017-09-09 00:09:34 -0400

    Add named views - master and detail

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,20 +1,56 @@
+/*
 (function () {
-      function config($stateProvider, $locationProvider) {
+      function config($stateProvider, $locationProvider,$urlRouterProvider) {
            $locationProvider
                .html5Mode({
                    enabled: true,
                    requireBase: false
                 });
 
-           $stateProvider
-               .state('home', {
-                   url: '/',
-                   controller: 'HomeCtrl as home',
-                   templateUrl: '/templates/home.html'
-               });
+          $stateProvider
+          .state('home', {
+              url: '/',
+              controller: 'HomeCtrl as home',
+              templateUrl: '/templates/home.html'
+          });
        }
 
     angular
-        .module('blocChat', ['ui.router', 'firebase', 'ui.bootstrap'])
+        .module('blocChat', ['ui.router', 'firebase'])
         .config(config);
 })();
+  */
+
+  (function () {
+        function config($stateProvider, $locationProvider,$urlRouterProvider) {
+             $locationProvider
+                 .html5Mode({
+                     enabled: true,
+                     requireBase: false
+                  });
+
+            $stateProvider
+            .state('home', {
+                url: '/',
+                views: {
+
+                  '': {
+                      templateUrl: '/templates/home.html',
+                      controller: 'HomeCtrl as home'
+                  },
+                  'master': {
+                      templateUrl: '/templates/home.html',
+                      controller: 'HomeCtrl as home'
+                  },
+                  'detail': {
+                      templateUrl: '/templates/roominfo.html',
+                      controller: 'HomeCtrl as home'
+                  }
+                }
+            });
+         }
+
+      angular
+          .module('blocChat', ['ui.router', 'firebase'])
+          .config(config);
+  })();
